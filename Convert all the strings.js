1e1e0274289f315ececba75f6729d042cfb784e9
Prabhu <123736381+Prabhu67@users.noreply.github.com>
2023-02-25 14:59:37 +0530

    Add files via upload

---
+++ Convert all the strings.js
@@ -0,0 +1,9 @@
+let stringArray = ["prabhu", "is", "a", "cricket", "player"]
+
+stringArray = stringArray.map((item) => {
+
+    return item.toUpperCase()
+
+})
+
+console.log("Converted String Array is: " + stringArray)
